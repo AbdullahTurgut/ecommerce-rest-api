ffd0aa05265e1dab30068d38872d95104fd62b5e
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-05-21 06:50:34 +0300

    WIP: image controller base structure

--- src/main/java/com/dailyalcorcode/buynowdotcom/controller/ImageController.java
+++ src/main/java/com/dailyalcorcode/buynowdotcom/controller/ImageController.java
@@ -1,4 +1,51 @@
 package com.dailyalcorcode.buynowdotcom.controller;
 
+
+import com.dailyalcorcode.buynowdotcom.dtos.ImageDto;
+import com.dailyalcorcode.buynowdotcom.model.Image;
+import com.dailyalcorcode.buynowdotcom.response.ApiResponse;
+import com.dailyalcorcode.buynowdotcom.service.image.IImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/images")
 public class ImageController {
+
+    private final IImageService imageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ApiResponse> uploadImages(@RequestParam("productId") Long productId,
+                                                    @RequestParam("files") List<MultipartFile> files) {
+        try {
+            List<ImageDto> imageDto = imageService.saveImages(productId, files);
+            return ResponseEntity.ok(new ApiResponse("Images upload successfully!", imageDto));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Upload error!", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+        Image image = imageService.getImageById(imageId);
+        ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"").body(resource);
+    }
+
 }
